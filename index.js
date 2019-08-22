f80d02e673642d7ea4b783ea8f4713d9ca9cfa38
Mouni Mullapudi <mullapudi24@gmail.com>
2019-08-22 16:07:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Code your solution in this file
+
+function findMatching(list, string){
+ return list.filter(function (driverNames) {
+    return driverNames.toLowerCase() === string.toLowerCase();
+  });
+}
+
+function fuzzyMatch(list, names){
+  return list.filter (function )
+}
